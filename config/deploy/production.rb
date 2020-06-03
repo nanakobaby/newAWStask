d4f6d555db263ecfc2a8c49aebf0b645d326a7b1
nanako kan <n.k.0213.0529@gmail.com>
2020-06-04 08:56:10 +0900

    init

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 server '54.168.27.111', user: 'app', roles: %w{app db web}
 set :ssh_options, {
-  port: 80,#XXXにポート番号
+  port: 22,#XXXにポート番号
   keys: [File.expand_path('/Users/kannanako/.ssh/id_rsa')], #'~/.ssh/id_rsa'部分をローカル環境の鍵のパスに
   forward_agent: true,
   auth_methods: %w(publickey)
